2a3fa7ab47049cf0be5484306766b53187232d0c
Saranga <amisaranga@gmail.com>
2015-10-21 15:44:55 +0530

    Modal content added.

--- plugin.js
+++ plugin.js
@@ -1,3 +1,5 @@
+var win
+
 tinymce.PluginManager.add('charts', function(editor, url) {
     // Add a button that opens a window
     editor.addButton('charts', {
@@ -26,18 +28,47 @@ tinymce.PluginManager.add('charts', function(editor, url) {
             });
         }
     });
+    
+    editor.on('init', function() {
+		
+	});
 
     var showModal = function(url) {
         // Open window
-        editor.windowManager.open({
-            title: 'charts plugin',
+        win = editor.windowManager.open({
+            title: 'Chart',
             body: [
-                {type: 'textbox', name: 'title', label: 'Title'}
+                {type: 'textbox', name: 'title', label: 'Title'},
             ],
+            width: 700,
+            height: 500,
             onsubmit: function(e) {
                 // Insert content when the window form is submitted
                 editor.insertContent('Title: ' + e.data.title);
             }
         });
+        
+        initModal(win);
     };
+    
+    var initModal = function(win) {
+		var id = win._id;
+		var body = win.$el.find('div#' + id + '-body')[0];
+		body.innerHTML = '<div>'
+			+ '<div id="' + id + '-charts-chart-container">'
+				+ '<canvas id="' + id + '-charts-chart"></canvas>'
+			+ '</div>'
+			+ '<div id="' + id + '-charts-data-container">'
+				+ '<h2>Data</h2>'
+				+ '<div id="' + id + '-charts-data-table-wrapper">'
+					+ '<table id="' + id + '-charts-data-table>'
+						+ ''
+					+ '</table>'
+				+ '</div>'
+			+ '</div>'
+			+ '<div id="' + id + '-charts-options-container">'
+				+ '<h2>Options</h2>'
+			+ '</div>'
+		+ '</div>';
+	};
 });
